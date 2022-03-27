4440adf97d40a193128cf8914700fda65702347c
vesso8 <skittles8@abv.bg>
2022-03-27 23:03:00 +0300

    Staticfiles_dirst changed

--- petstagram/settings.py
+++ petstagram/settings.py
@@ -1,4 +1,4 @@
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -121,10 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = (BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR/ 'staticfiles')
+
 STATICFILES_DIRS = (
-    BASE_DIR/ 'static',
+   os.path.join(BASE_DIR / 'static'),
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
